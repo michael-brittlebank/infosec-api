cd7644410b299a390487c387ea0a08261147cd78
mike-stumpf <mike.a.stumpf@gmail.com>
2018-06-24 16:37:19 -0500

    only request specific fields from the countries api to make response and cache sizes smaller

--- src/services/countries.service.php
+++ src/services/countries.service.php
@@ -11,8 +11,9 @@ class CountriesService {
 
         if (is_null($this->countryList)) {
             try {
+                // since countries change very infrequently and therefore this call has a non-variable response size it is easier to store it in a local cache and perform filtering, sorting, and pagination operations on it ourselves
                 $pest = new \Pest('https://restcountries.eu');
-                $this->countryList = json_decode($pest->get('/rest/v2/all'), true);
+                $this->countryList = json_decode($pest->get('/rest/v2/all?fields=name;alpha2Code;alpha3Code;flag;region;subregion;population;languages'), true);
                 return $this->countryList;
             } catch (\Pest_BadRequest $e) {
                 $this->logger->error($e);
